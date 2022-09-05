f268f394473d66513b5446dd540b1bbb3ca9c866
Ilua <43353678+Fluidios@users.noreply.github.com>
2022-09-05 20:08:29 +0300

    HW 3.6

---
+++ 3.6 HW/3.6 HW/Program.cs
@@ -0,0 +1,30 @@
+﻿//Выполнил Котельников И.А. 05.09.2022 version 1
+
+#region Task 1
+string _fullName = "Котельников Илья Андреевич";
+int _age = 22;
+string _email = "i.kotelnikov@list.ru";
+float _programmingPoints = 79.3f;
+float _mathPoints = 76.2f;
+float _physicsPoints = 60.2f;
+
+Console.WriteLine(string.Format("Ф.И.О. - {0}", _fullName));
+Console.WriteLine(string.Format("Возраст - {0}", _age));
+Console.WriteLine(string.Format("Электронная почта - {0}", _email));
+Console.WriteLine(string.Format("Баллы по программированию - {0}", _programmingPoints));
+Console.WriteLine(string.Format("Баллы по математике - {0}", _mathPoints));
+Console.WriteLine(string.Format("Баллы по физике - {0}", _physicsPoints));
+#endregion
+
+#region Task 2
+float pointsSumm;
+float avearagePointsValue;
+
+pointsSumm = _programmingPoints + _mathPoints + _physicsPoints;
+avearagePointsValue = pointsSumm / 3;
+
+Console.ReadLine();
+Console.WriteLine(string.Format("Сумма баллов по всем предметам - {0}", pointsSumm));
+Console.ReadLine();
+Console.WriteLine(string.Format("Средний балл - {0}", avearagePointsValue));
+#endregion

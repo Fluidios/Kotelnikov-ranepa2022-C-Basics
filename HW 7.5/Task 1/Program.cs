80aecb5200630142c902a2b1442c05078457a5e1
Ilua <43353678+Fluidios@users.noreply.github.com>
2022-09-30 11:24:58 +0300

    HW 7.5

---
+++ HW 7.5/Task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿string input = Console.ReadLine();
+
+ShowOneByOne(SplitToWords(input));
+
+
+string[] SplitToWords(string input)
+{
+    string[] split = input.Split(" ");
+    return split;
+}
+
+void ShowOneByOne(string[] toShow)
+{
+    foreach (var item in toShow)
+        Console.WriteLine(item);
+}
---
+++ HW 7.5/Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿string input = Console.ReadLine();
+
+Console.WriteLine(Reverse(input));
+
+
+string Reverse(string input)
+{
+    string result = string.Empty;
+    foreach (var item in SplitToWords(input).Reverse())
+        result += item + " ";
+    return result;
+}
+
+string[] SplitToWords(string input)
+{
+    string[] split = input.Split(" ");
+    return split;
+}

3bb7f0d7709132969a276b7ee806831a4942227e
Ilua <43353678+Fluidios@users.noreply.github.com>
2022-10-21 13:05:27 +0300

    HW 15.6 v2

--- HW 15.6/Task 4/Program.cs
+++ HW 15.6/Task 4/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.Serialization;
+﻿using System.IO;
+using System.Text;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 CreateNewPerson(out Person p);
 SaveToFile(p);
@@ -6,12 +9,14 @@ SaveToFile(p);
 
 string FilePath() {  return Path.Combine(Environment.CurrentDirectory, "XmlFile.xml");  }
 
-void SaveToFile<T>(T data)
+void SaveToFile(Person data)
 {
-    using (TextWriter sw = File.CreateText(FilePath()))
+    using(StreamWriter sw = File.CreateText(FilePath()))
     {
-        XmlSerializer xs = new XmlSerializer(typeof(T));
-        xs.Serialize(sw, data);
+        XElement root = new XElement(data.Fio);
+        root.Add(ToXElement(data.Address));
+        root.Add(ToXElement(data.Phones));
+        root.Save(sw);
     }
 }
 
@@ -25,6 +30,20 @@ void CreateNewPerson( out Person person)
     string stationaryP = RequestString("Введите домашний номер:");
 
     person = new Person(fio, street, house, flat, mobileP, stationaryP);
+}
+
+XElement ToXElement<T>(T data)
+{
+
+    using (var memoryStream = new MemoryStream())
+    {
+        using (TextWriter streamWriter = new StreamWriter(memoryStream))
+        {
+            var xmlSerializer = new XmlSerializer(typeof(T));
+            xmlSerializer.Serialize(streamWriter, data);
+            return XElement.Parse(Encoding.ASCII.GetString(memoryStream.ToArray()));
+        }
+    }
 }
 
 string RequestString (string requestString)
@@ -57,19 +76,40 @@ bool TryConvertToInt(string source, string wrongSourceMessage, out int result)
 public struct Person
 {
     public string Fio;
-    public string Street;
-    public int HouseNumber;
-    public int FlatNumber;
-    public string MobilePhone;
-    public string StationaryPhone;
+    public AddressContainer Address;
+    public PhonesContainer Phones;
 
     public Person(string fio, string street, int houseNumber, int flatNumber, string mobilePhone, string stationaryPhone)
     {
         Fio = fio;
-        Street = street;
-        HouseNumber = houseNumber;
-        FlatNumber = flatNumber;
-        MobilePhone = mobilePhone;
-        StationaryPhone = stationaryPhone;
+        Address = new AddressContainer(street, houseNumber, flatNumber);
+        Phones = new PhonesContainer(mobilePhone, stationaryPhone);
+    }
+
+    [Serializable]
+    public struct AddressContainer
+    {
+        public string Street;
+        public int HouseNumber;
+        public int FlatNumber;
+
+        public AddressContainer(string street, int houseNumber, int flatNumber)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            FlatNumber = flatNumber;
+        }
+    }
+    [Serializable]
+    public struct PhonesContainer
+    {
+        public string MobilePhone;
+        public string StationaryPhone;
+
+        public PhonesContainer(string mobilePhone, string stationaryPhone)
+        {
+            MobilePhone = mobilePhone;
+            StationaryPhone = stationaryPhone;
+        }
     }
 }
